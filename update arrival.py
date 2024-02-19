5ff70eccca34ef573223fbf020a7d8fab28c1700
Manuel Alejandro <manuel.alejandro.nolasco@estudiantat.upc.edu>
2024-02-19 08:15:44 -0700

    primera version de update_arrival

--- update arrival.py
+++ update arrival.py
@@ -1,5 +1,5 @@
 def update_arrival(h,m,d):
-    ""
+    """
     >> > update_arrival(10, 45, 20)
     (11, 5)
     >> > update_arrival(12, 30, 60)
@@ -8,7 +8,7 @@ def update_arrival(h,m,d):
     (0, 0)
     >> > update_arrival(23, 57, 5 + 24 * 60)
     (0, 2)
-
+    """
     mt= h * 60 +m+d
     nh = mt//60 % 24
     nm = mt % 60
